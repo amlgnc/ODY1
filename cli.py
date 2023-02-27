8a9271eb45c12149473376eff708c652f51e34fd
amlgnc <alex2002hiphop@gmail.com>
2023-02-27 22:00:46 +0100

    CLI

--- cli.py
+++ cli.py
@@ -1,23 +1,18 @@
 import requests
 import zipfile
+import shutil
 import io
 import os
 
 
-def dlWindows():
-    print("dl GCC 7.3.0 MinGW (DW2) - 32-bit")
-    r = requests.get("https://www.sfml-dev.org/files/SFML-2.5.1-windows-gcc-7.3.0-mingw-32-bit.zip")
+def dl(name, href):
+    print(f"\n> import {name}")
+    r = requests.get(href)
     z = zipfile.ZipFile(io.BytesIO(r.content))
-    z.extractall("")
-    os.rename("SFML-2.5.1","SFML")
-    exit()
-
-
-def dlLinux():
-    exit()
-
-
-def dlMacOS():
+    z.extractall('')
+    if os.path.exists('SFML'):
+        shutil.rmtree('SFML')
+    os.rename('SFML-2.5.1','SFML')
     exit()
 
 
@@ -26,11 +21,11 @@ while True:
     cmd = input()
     if cmd == 'exit':
         exit()
-    elif cmd == "w":
-        dlWindows()
-    elif cmd == "l":
-        pass
-    elif cmd == "m":
-        pass
+    elif cmd == 'win':
+        dl('GCC 7.3.0 MinGW (DW2) - 32-bit', 'https://www.sfml-dev.org/files/SFML-2.5.1-windows-gcc-7.3.0-mingw-32-bit.zip')
+    elif cmd == 'lin':
+        print("\n> No.\n") # TODO
+    elif cmd == 'mac':
+        print("\n> No.\n") # TODO
     else:
-        print("Invalid command")
+        print("\n> No.\n")

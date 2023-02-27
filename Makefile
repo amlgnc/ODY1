4876fa1a1f7b74fe65892b4d34dc898cf2f63894
amlgnc <alex2002hiphop@gmail.com>
2023-02-27 23:29:53 +0100

    test

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ compile:
 	g++ -c src/main.cpp -ISFML/include -o SFML/bin/main.o
 
 link:
-	g++ SFML/bin/main.o -o SFML/bin/main -LSFML/lib -lsfml-graphics -lsfml-window -lsfml-system
+	g++ SFML/bin/main.o -o SFML/bin/main -LSFML/lib -lsfml-audio -lsfml-graphics -lsfml-window -lsfml-system
 
 run:
 	./SFML/bin/main
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,24 @@
-#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    sf::Window window(sf::VideoMode(800, 600), "ODY1");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "ODY1");
+
+    sf::Texture texture;
+    if (!texture.loadFromFile("SFML/bin/test.jpg"))
+        return EXIT_FAILURE;
+    sf::Sprite sprite(texture);
+
+    sf::Font font;
+    if (!font.loadFromFile("SFML/bin/test.ttf"))
+        return EXIT_FAILURE;
+    sf::Text text("Yay!", font, 50);
+
+    sf::Music music;
+    if (!music.openFromFile("SFML/bin/test.ogg"))
+        return EXIT_FAILURE;
+    // music.play();
 
     // Main loop
     while (window.isOpen())
@@ -10,10 +26,14 @@ int main()
         sf::Event event;
         while (window.pollEvent(event))
         {
+            // Close window: exit
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+        window.clear();
+        window.draw(sprite);
+        window.draw(text);
+        window.display();
     }
-
-    return 0;
+    return EXIT_SUCCESS;
 }
